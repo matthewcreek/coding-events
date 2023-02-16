7a6a91a958a8d85f3ee126f4bc4a3dac33b5759c
Matthew Creek <matthew.creek27@gmail.com>
2023-02-16 16:33:56 -0600

    added exercises code

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,16 +8,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events/")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+//    private static List<String> events = new ArrayList<>();
+
+//    @GetMapping("/")
+//    public String displayAllEvents(Model model) {
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
 
     @GetMapping("/")
     public String displayAllEvents(Model model) {
+        HashMap<String, String> events = new HashMap<>();
+        events.put("Code Til Dawn", "Join friends to write code late into the evening it is fun");
+        events.put("Free Coffee Day", "Come to the coffee truck and slurp that hot brown");
+        events.put("Bill's Big Day", "Birthday party for 'Bill'");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -29,7 +40,7 @@ public class EventController {
     //path /events/create
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+//        events.add(eventName);
         return "redirect:";
     }
 
