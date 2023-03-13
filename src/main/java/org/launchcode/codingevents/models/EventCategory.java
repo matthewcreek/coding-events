37142daa903d5745b3583522020ed93d4b05315b
Matthew Creek <matthew.creek27@gmail.com>
2023-03-13 13:39:01 -0500

    trying to troubleshoot error messages

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,14 +1,11 @@
 package org.launchcode.codingevents.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Size;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 public class EventCategory extends AbstractEntity {
